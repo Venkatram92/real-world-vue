4030de990f0de855516c5cda17d8306982916e15
Venkatram92 <venkatndd271@gmail.com>
2020-03-17 23:40:48 +0530

    Move vuex to route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import EventCreate from "../views/EventCreate.vue";
 import EventList from "../views/EventList.vue";
 import EventShow from "../views/EventShow.vue";
+import NProgress from "nprogress";
+import store from "@/store/index.js";
 
 Vue.use(VueRouter);
 
@@ -21,13 +23,28 @@ const routes = [
     path: "/getevent/:id",
     name: "event-show",
     component: EventShow,
-    props: true
+    props: true,
+    beforeEnter(routeTo, routeFrom, next) {
+      store.dispatch("event/fetchEvent", routeTo.params.id).then(event => {
+        routeTo.params.event = event;
+        next();
+      });
+    }
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes
+});
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
